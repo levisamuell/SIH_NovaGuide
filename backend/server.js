67e8210a609aeea0dbc164ce74a2450061363700
levisamuell <demolitiongaming2005@gmail.com>
2025-08-05 09:38:03 +0530

    resolved cors error

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+  origin: "https://sih-nova-guide.vercel.app", 
+}));
 app.use(express.json());
 
 const systemPrompt = `
